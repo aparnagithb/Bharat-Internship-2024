518c2eee4975cc2629a7c3e39ffa5fce8248951b
aparnagithb <mapa22079.cs@rmkec.ac.in>
2024-01-28 12:34:59 +0530

    first commit

---
+++ server/server.py
@@ -0,0 +1,27 @@
+from flask import Flask , request, jsonify
+import util
+app=Flask(__name__)
+@app.route('/get_locations')
+def get_locations():
+    response =jsonify({
+        'locations':util.get_locations()
+    })
+
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
+@app.route('/predict_home_price', methods=['POST'])
+def predict_homeprice():
+    total_sqft=float(request.form['total_sqft'])
+    location=request.form['location']
+    bhk=int(request.form['bhk'])
+    bath=int(request.form['bath'])
+
+    response=jsonify({
+        'estimated_price':util.get_estimated_price(location, total_sqft, bhk, bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
+
+if __name__ =="__main__":
+    print("starting")
+    app.run(debug=True)
